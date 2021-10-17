35c3bed036ad20c08ed18e44941ed7d544f5e759
LeonLit <leonlit123@gmail.com>
2021-10-17 22:47:16 +0800

    working on nav bar

---
+++ index.js
@@ -0,0 +1,9 @@
+function open_nav () {
+    const items = document.getElementsByClassName("nav_items")[0];
+    items.style.marginTop = "-5vh";
+}
+
+function close_nav () {
+    const items = document.getElementsByClassName("nav_items")[0];
+    items.style.marginTop = "-5vh";
+}
