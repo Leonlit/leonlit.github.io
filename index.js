a2bf72b022a862de5f382ee07cd72991d42723a7
LeonLit <leonlit123@gmail.com>
2021-10-18 12:11:47 +0800

    created initial nav bar and working on tags for projects

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const navTrigger = document.getElementById("nav-trigger");
+
+window.onload = function () {
+    navTrigger.addEventListener("click", open_nav, true);
+    console.log("test");
+}
+
 function open_nav () {
+    console.log("test2");
     const items = document.getElementsByClassName("nav_items")[0];
-    items.style.marginTop = "-5vh";
+    items.style.top = "60px";
+    navTrigger.removeEventListener("click", open_nav, true)
+    navTrigger.addEventListener("click", close_nav, true)
 }
 
 function close_nav () {
+    console.log("test3");
     const items = document.getElementsByClassName("nav_items")[0];
-    items.style.marginTop = "-5vh";
+    items.style.top = "-50vh";
+    navTrigger.removeEventListener("click", close_nav, true)
+    navTrigger.addEventListener("click", open_nav, true)
 }
