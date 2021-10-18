1e364bf2d4b342dce3a50cde9911dd3e511179f6
LeonLit <leonlit123@gmail.com>
2021-10-18 12:36:17 +0800

    cleaned up some js

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const navTrigger = document.getElementById("nav-trigger");
 
 window.onload = function () {
     navTrigger.addEventListener("click", open_nav, true);
-    console.log("test");
 }
 
 function open_nav () {
-    console.log("test2");
     const items = document.getElementsByClassName("nav_items")[0];
     items.style.top = "60px";
     navTrigger.removeEventListener("click", open_nav, true)
@@ -14,7 +12,6 @@ function open_nav () {
 }
 
 function close_nav () {
-    console.log("test3");
     const items = document.getElementsByClassName("nav_items")[0];
     items.style.top = "-50vh";
     navTrigger.removeEventListener("click", close_nav, true)
