a0f895ed6049596342ec8798c1a355ced97d6e21
LeonLit <leonlit123@gmail.com>
2021-10-19 15:25:56 +0800

    added gallery to the application(still working on making it more user friendly

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const navTrigger = document.getElementById("nav-trigger");
+const gallery = document.getElementsByClassName("images-container")[0];
+let galleryCount = 0;
+let timerHandler;
 
 window.onload = function () {
     navTrigger.addEventListener("click", open_nav, true);
+    start_gallery_timer();
+}
+
+function start_gallery_timer () {
+    timerHandler = setInterval(rotateGallery, 1000 * 10);
+}
+
+function remove_gallery_timer () {
+    clearInterval(timerHandler);
 }
 
 function open_nav () {
@@ -16,4 +28,10 @@ function close_nav () {
     items.style.top = "-50vh";
     navTrigger.removeEventListener("click", close_nav, true)
     navTrigger.addEventListener("click", open_nav, true)
+}
+
+function rotateGallery () {
+    console.log("test");
+    galleryCount++;
+    gallery.style.marginLeft = "" + (-1 * (galleryCount % 3) * 100 ) + "vw";
 }
